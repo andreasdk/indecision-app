0b056f4b4fe43e9f0f185d36b62eb6da93d24581
Andrew Kennedy <andrew.kennedy27@outlook.com>
2018-06-04 15:33:18 +0200

    es6 classes playground

---
+++ src/playground/es6-class-1.js
@@ -0,0 +1,58 @@
+class Person {
+	constructor(name = 'Anonymous', age = 0) {
+		this.name = name;
+		this.age = age;
+	}
+	getGreeting() {
+		//return 'Hi. I am ' + this.name + '!';
+		return `Hi. I am ${this.name}!`;
+	}
+	getDescription() {
+		return 	`${this.name} is ${this.age} year(s) old.`
+	}
+}
+
+class Student extends Person {
+	constructor(name, age, major) {
+		super(name, age);
+		this.major = major;
+	}
+	hasMajor() {
+		return !!this.major;
+	}
+	getDescription() {
+		let description = super.getDescription();
+
+		if (this.hasMajor()) {
+			description += ` Their major is ${this.major}`;
+		}
+		return description;
+	}
+}
+
+class Traveller extends Person {
+	constructor(name, age, hometown) {
+		super(name, age);
+		this.hometown = hometown;
+	}
+	homeLocation() {
+		return !!this.hometown;
+	}
+	getGreeting() {
+		let greeting = super.getGreeting();
+
+		if (this.homeLocation()) {
+			greeting += ` I'm visiting from ${this.hometown}`;
+		}
+		return greeting;
+	}
+}
+
+const me = new Traveller('Andrew Kennedy', 29, 'Dublin');
+
+console.log(me.getGreeting());
+
+
+const other = new Student();
+console.log(other.getDescription());
+
