14bd4f1b723ee04173514ab8fc95ba76846f7487
Andrew Kennedy <andrew.kennedy27@outlook.com>
2018-06-13 23:09:31 +0200

    state to indecision app

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,26 +1,36 @@
-let visibility = false;
-
-const toggleVisibility = () => {
-	visibility = !visibility;
-	render();
-};
-
-const render = () => {
-	const jsx = (
-		<div>
+class VisibilityToggle extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+		this.state = {
+			visibility: false
+		};
+	}
+	handleToggleVisibility() {
+		this.setState((prevState) => {
+			return {
+				visibility: !prevState.visibility
+			};
+		});
+	}
+	render() {
+		return (
+		  <div>
 			<h1>Visibility Toggle</h1>
-			<button onClick={toggleVisibility}>
-			  {visibility ? 'Hide details' : 'Show details'}
+			<button onClick={this.handleToggleVisibility}>
+			  {this.state.visibility ? 'Hide details' : 'Show details'}
 			</button>
-			{visibility && (
+			{this.state.visibility && (
 				<div>
 				 <p>Some details</p>
 				</div>
 			)}
-		</div>
-	);
+		  </div>
+	    );
+	}
+}
 
-	ReactDOM.render(jsx, document.getElementById('app'));
-};
 
-render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
+
+
